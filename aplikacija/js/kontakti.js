121e7ad55c755c059a76f36f1dd7d97a87dc0bb4
Mihael Baranašić <mbaranasic100@gmail.com>
2024-07-01 13:40:13 +0200

    Napravljeno pretraživanje kontakata

--- aplikacija/js/kontakti.js
+++ aplikacija/js/kontakti.js
@@ -1,12 +1,15 @@
 let url = "http://localhost:3000";
+let sviKorisnici = [];
+let kontakti = [];
 
-window.addEventListener('load', async ()=>{
-    ucitaj();
-})
+window.addEventListener('load', async () => {
+    await ucitaj();
+    document.getElementById('pretraga-korisnici').addEventListener('input', pretraziKorisnike);
+});
 
-async function ucitaj(){
-    ucitajKontakte();
-    ucitajSveKorisnike();
+async function ucitaj() {
+    await ucitajKontakte();
+    await ucitajSveKorisnike();
 }
 
 async function ucitajKontakte() {
@@ -18,16 +21,11 @@ async function ucitajKontakte() {
         }
     };
     let odgovor = await fetch(`${url}/baza/kontakti/${korime}`, parametri);
-    let korisnici = await odgovor.json();
-    let popisKontakata = document.getElementById('listaKontakata');
-    let html = ""
-    for (let korisnik of korisnici) {
-        html += "<li>" + korisnik.kontakt_korime + "</li>";
-    }
-    popisKontakata.innerHTML = html;
+    kontakti = await odgovor.json();
+    prikaziKontakte(kontakti);
 }
 
-async function ucitajSveKorisnike(){
+async function ucitajSveKorisnike() {
     let korime = document.getElementById('korime').innerHTML;
     let parametri = {
         method: "GET",
@@ -36,28 +34,49 @@ async function ucitajSveKorisnike(){
         }
     };
     let odgovor = await fetch(`${url}/baza/nisuKontakti/${korime}`, parametri);
-    let korisnici = await odgovor.json();
+    sviKorisnici = await odgovor.json();
+    prikaziSveKorisnike(sviKorisnici);
+}
+
+function prikaziKontakte(kontakti) {
+    let popisKontakata = document.getElementById('listaKontakata');
+    let html = "";
+    for (let korisnik of kontakti) {
+        html += `<li>${korisnik.kontakt_korime}</li>`;
+    }
+    popisKontakata.innerHTML = html;
+    document.getElementById('broj-kontakata').innerText = kontakti.length;
+}
+
+function prikaziSveKorisnike(korisnici) {
     let popisKorisnikaHTML = document.getElementById('sviKorisnici');
     let html = "";
     for (let korisnik of korisnici) {
         html += `<li onclick='dodajKontakt("${korisnik.id}", "${korime}")'>${korisnik.korime}</li>`;
-        console.log(korisnik.id);
     }
     popisKorisnikaHTML.innerHTML = html;
 }
 
-async function dodajKontakt(korisnik, korime) {
+async function dodajKontakt(korisnikId, korime) {
     let parametri = {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ korisnik })
+        body: JSON.stringify({ korisnik: korisnikId })
     };
     let odgovor = await fetch(`${url}/baza/kontakti/${korime}`, parametri);
-    if(odgovor.status == 201){
-        ucitaj();
-    }else{
+    if (odgovor.status == 201) {
+        await ucitaj();
+    } else {
         console.error("Greška kod dodavanja kontakta!");
     }
+}
+
+function pretraziKorisnike() {
+    let pretraga = document.getElementById('pretraga-korisnici').value.toLowerCase();
+    let filtriraniKontakti = kontakti.filter(korisnik => korisnik.kontakt_korime.toLowerCase().includes(pretraga));
+    let filtriraniKorisnici = sviKorisnici.filter(korisnik => korisnik.korime.toLowerCase().includes(pretraga));
+    prikaziKontakte(filtriraniKontakti);
+    prikaziSveKorisnike(filtriraniKorisnici);
 }
