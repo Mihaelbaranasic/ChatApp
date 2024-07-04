0208cdc4eba503ae3c2813e8461552481e5587c4
Mihael Baranašić <mbaranasic100@gmail.com>
2024-07-04 19:19:21 +0200

    Dodana DAO datoteka

---
+++ servis/upravljanjeBazom/datotekaDAO.js
@@ -0,0 +1,30 @@
+const Baza = require("./baza.js");
+
+class DatotekaDAO {
+    constructor() {
+        this.baza = new Baza("/home/NPK_01/mbaranasi21/ChatApp/servis/baza/ChatApp_baza.sqlite");
+    }
+
+    async dajDatoteke(posiljatelj, primatelj) {
+        this.baza.spojiSeNaBazu();
+        let sql = `SELECT d.*, k.korime
+                   FROM datoteka d
+                   JOIN korisnik k ON d.korisnik_id = k.id
+                   WHERE (korisnik_id = (SELECT id FROM korisnik WHERE korime = ?) AND kontakt_id = (SELECT id FROM korisnik WHERE korime = ?)) 
+                      OR (korisnik_id = (SELECT id FROM korisnik WHERE korime = ?) AND kontakt_id = (SELECT id FROM korisnik WHERE korime = ?))
+                   ORDER BY vrijemePrimitka`;
+        let podaci = await this.baza.izvrsiUpit(sql, [posiljatelj, primatelj, primatelj, posiljatelj]);
+        this.baza.zatvoriVezu();
+        return podaci;
+    }
+
+    async posaljiDatoteku(posiljatelj, primatelj, nazivDatoteke, putanjaDatoteke) {
+        this.baza.spojiSeNaBazu();
+        let sql = `INSERT INTO datoteka (korisnik_id, kontakt_id, naziv, putanja, vrijemePrimitka) 
+                   VALUES ((SELECT id FROM korisnik WHERE korime = ?), (SELECT id FROM korisnik WHERE korime = ?), ?, ?, datetime('now'))`;
+        await this.baza.izvrsiUpit(sql, [posiljatelj, primatelj, nazivDatoteke, putanjaDatoteke]);
+        this.baza.zatvoriVezu();
+    }
+}
+
+module.exports = DatotekaDAO;
