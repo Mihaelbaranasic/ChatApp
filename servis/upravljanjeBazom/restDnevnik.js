a50bfc52b7be5e6894146b368206dff84b5f34b0
Mihael Baranašić <mbaranasic100@gmail.com>
2024-08-12 13:47:50 +0200

    Reinicijalizacija repozitorija nakon brisanja corupt datoteka

---
+++ servis/upravljanjeBazom/restDnevnik.js
@@ -0,0 +1,64 @@
+const DnevnikDAO = require("./dnevnikDAO.js");
+
+exports.getDnevnik = function (zahtjev, odgovor) {
+    odgovor.type("application/json");
+    let ddao = new DnevnikDAO();
+    ddao.dajSve().then((zapisi) => {
+        odgovor.status(200);
+        odgovor.send(JSON.stringify(zapisi));
+    }).catch((error) => {
+        odgovor.status(500);
+        odgovor.send(JSON.stringify({ greska: "Greška pri dohvaćanju zapisa iz dnevnika." }));
+    });
+};
+
+exports.getDnevnikKorisnika = function (zahtjev, odgovor) {
+    odgovor.type("application/json");
+    let ddao = new DnevnikDAO();
+    let korime = zahtjev.params.korime;
+    ddao.daj(korime).then((zapis) => {
+        if (zapis) {
+            odgovor.status(200);
+            odgovor.send(JSON.stringify(zapis));
+        } else {
+            odgovor.status(404);
+            odgovor.send(JSON.stringify({ greska: "Zapis nije pronađen." }));
+        }
+    }).catch((error) => {
+        odgovor.status(500);
+        odgovor.send(JSON.stringify({ greska: "Greška pri dohvaćanju zapisa iz dnevnika." }));
+    });
+};
+
+exports.postDnevnik = function (zahtjev, odgovor) {
+    odgovor.type("application/json");
+    let ddao = new DnevnikDAO();
+    let { korisnik, aktivnost } = zahtjev.body;
+    ddao.dodaj(korisnik, aktivnost).then(() => {
+        odgovor.status(201);
+        odgovor.send(JSON.stringify({ poruka: "Zapis dodan u dnevnik." }));
+    }).catch((error) => {
+        odgovor.status(500);
+        odgovor.send(JSON.stringify({ greska: "Greška pri dodavanju zapisa u dnevnik." }));
+    });
+};
+
+exports.deleteDnevnikKorisnika = function (zahtjev, odgovor) {
+    odgovor.type("application/json");
+    let ddao = new DnevnikDAO();
+    let korime = zahtjev.params.korime;
+    ddao.obrisi(korime).then(() => {
+        odgovor.status(200);
+        odgovor.send(JSON.stringify({ poruka: "Zapis izbrisan iz dnevnika." }));
+    }).catch((error) => {
+        odgovor.status(500);
+        odgovor.send(JSON.stringify({ greska: "Greška pri brisanju zapisa iz dnevnika." }));
+    });
+};
+
+exports.putDnevnik = function (zahtjev, odgovor) {
+	odgovor.type("application/json");
+	odgovor.status(405);
+	let poruka = { greska: "Zabranjeno" };
+	odgovor.send(JSON.stringify(poruka));
+};
